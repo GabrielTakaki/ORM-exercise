0b2b2ca775064f616953e3f0d873060cf3c0c423
Gabriel Takaki <gabitj@icloud.com>
2022-01-04 18:42:08 -0300

    update functionaity created

--- controllers/books/index.js
+++ controllers/books/index.js
@@ -5,5 +5,6 @@ const router = express.Router({ mergeParams: true });
 router.get('/', require('./list'));
 router.get('/:id', require('./findById'));
 router.post('/', require('./create'));
+router.put('/:id', require('./update'));
 
 module.exports = router;
--- controllers/books/update.js
+++ controllers/books/update.js
@@ -2,8 +2,17 @@ const { Books } = require('../../models');
 
 module.exports = async (req, res) => {
   try {
-    
+    const { author, title, pageQuantity } = req.body;
+    const { id } = req.params;
 
+    const [updateBook] = await Books.update(
+      { author, title, pageQuantity },
+      { where: { id } },
+    );
+
+    if (!updateBook) return res.status(404).json({ message: 'Livro não encontrado' });
+
+    return res.status(200).json({ message: 'Livro atualizado com sucesso!' });
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
