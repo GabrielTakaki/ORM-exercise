791401b09652436da1962e9c1a8b317f095e2584
Gabriel Takaki <gabitj@icloud.com>
2022-01-04 18:27:16 -0300

    create function

---
+++ controllers/books/create.js
@@ -0,0 +1,14 @@
+const { Books } = require('../models');
+
+module.exports = async (req, res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+
+    const newBook = await Books.create({ title, author, pageQuantity });
+
+    return res.status(201).json(newBook);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}!`);
+});
